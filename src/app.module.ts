093bbc9a460096b8e88462e657f955670994a850
Alejandro Cuello <acuellodemoya@gmail.com>
2022-10-09 17:03:55 -0500

    Connection with mongoDb is Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path'
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -8,6 +9,9 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule
   ],
 })
@@ -20,4 +24,9 @@ export class AppModule {}
  * if the error "Cannot find module 'path'"
  * appears, then install the package '@types/node'
  * in the dependencies, not in devDependencies.
+ */
+
+/**
+ * Remember to update the tsc and Typewcript on
+ * your machine before to use 'MongooseModule'
  */
